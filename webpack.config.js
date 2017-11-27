022546f6f7f479aca7f64c662c604dcad49be07b
michaldrabikowski <drabikowski@gmail.com>
2017-11-27 21:13:24 +0100

    +add: i18n support

--- webpack.config.js
+++ webpack.config.js
@@ -2,51 +2,68 @@ const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const I18nPlugin = require('i18n-webpack-plugin');
 
-module.exports = {
-    context: __dirname,
-    entry: './src/app/main.js',
-    output: {
-        path: path.join(__dirname, "dist"),
-        filename: 'js/bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|jpe?g|png|gif|svg|pdf)$/,
-                loader: 'file-loader'
-            },
+const languages = {
+    "en": require("./src/i18n/en.json"),
+    "pl": require("./src/i18n/pl.json")
+};
 
-            // transform ES6 to ES5
-            {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        babelrc: true,
+const defaultLanguage = "en";
+
+
+module.exports = Object.keys(languages).map((language) => {
+
+    return {
+
+        context: __dirname,
+        entry: './src/app/main.js',
+        output: {
+            path: path.join(__dirname, "dist"),
+            filename: 'js/bundle.js'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(eot|svg|ttf|woff|woff2|jpe?g|png|gif|svg|pdf)$/,
+                    loader: 'file-loader'
+                },
+
+                // transform ES6 to ES5
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            babelrc: true,
+                        }
                     }
+
+                },
+                {
+                    test: /\.scss$/,
+                    use: ExtractTextPlugin.extract({fallback: "style-loader", use: ["css-loader", "sass-loader"]})
                 }
+            ]
+        },
+        plugins: [
+            new I18nPlugin(
+                languages[language]
+            ),
+            new CopyWebpackPlugin([
+                {from: './src/assets', to: 'assets'},
+            ]),
 
-            },
-            {
-                test: /\.scss$/,
-                use: ExtractTextPlugin.extract({fallback: "style-loader", use: ["css-loader", "sass-loader"]})
-            }
-        ]
-    },
-    plugins: [
-        new CopyWebpackPlugin([
-            {from: './src/assets', to: 'assets'},
-        ]),
-
-        new ExtractTextPlugin('main.css'),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'about.html',
-            template: "./src/about.html"
-        })
-    ],
-}
+            new ExtractTextPlugin('main.css'),
+            new HtmlWebpackPlugin({
+                filename: language == defaultLanguage ? 'index.html' : `index-${language}.html`,
+                template: "./src/index.html"
+            }),
+            new HtmlWebpackPlugin({
+                filename: language == defaultLanguage ? 'about.html' : `about-${language}.html`,
+                template: "./src/about.html"
+            })
+        ],
+    }
+})
