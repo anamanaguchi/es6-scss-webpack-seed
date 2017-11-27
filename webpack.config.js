c9b9652dc0ce9ab617117672d0b245c81dc1effd
michaldrabikowski <drabikowski@gmail.com>
2017-11-27 20:12:53 +0100

    +add: preset env support && assets support

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     context: __dirname,
@@ -11,22 +12,34 @@ module.exports = {
     },
     module: {
         rules: [
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|jpe?g|png|gif|svg|pdf)$/,
+                loader: 'file-loader'
+            },
+
             // transform ES6 to ES5
             {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: true,
+                    }
                 }
+
             },
             {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract({ fallback: "style-loader", use: ["css-loader", "sass-loader"] })
+                use: ExtractTextPlugin.extract({fallback: "style-loader", use: ["css-loader", "sass-loader"]})
             }
         ]
     },
     plugins: [
+        new CopyWebpackPlugin([
+            {from: './src/assets', to: 'assets'},
+        ]),
+
         new ExtractTextPlugin('main.css'),
         new HtmlWebpackPlugin({
             template: "./src/index.html"
